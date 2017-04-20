7d2430a6c331af08ab364c0b7916f6b733c0347c
Krzysztof Sakowski <krz.sakowski@gmail.com>
2017-04-20 20:54:03 +0200

    Added simple biopython test for CI.

---
+++ tests/test_biopython.py
@@ -0,0 +1,12 @@
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+
+def test_sequence():
+    seq = Seq("AUAU", IUPAC.unambiguous_dna)
+    assert(seq.count("A") == seq.count("U"))
+
+
+def test_sequence_2():
+    seq = Seq("AUAUA", IUPAC.unambiguous_dna)
+    assert(seq.count("A") != seq.count("U"))
